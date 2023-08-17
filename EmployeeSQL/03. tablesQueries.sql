1380caa9cbfb48cf10a3b504394a692dc606db74
HikmetYigit34 <info@controlsystemdesign.io>
2023-08-16 22:15:03 -0400

    table PK and FK updates

--- EmployeeSQL/03. tablesQueries.sql
+++ EmployeeSQL/03. tablesQueries.sql
@@ -6,21 +6,21 @@ DROP TABLE IF EXISTS employees;
 DROP TABLE IF EXISTS salaries;
 DROP TABLE IF EXISTS titles;
 
---Creating Tables----------------------------------------------------------------------------------
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
 
---Step01 create table------------------------------------------------------------------------------
-CREATE TABLE "titles" (
-    "title_id" VARCHAR(10)   NOT NULL,
-    "title" VARCHAR(30)   NOT NULL,
-    CONSTRAINT "pk_titles" PRIMARY KEY (
-        "title_id"
+--create table-------------------------------------------------------------------------------------
+CREATE TABLE "dept_manager" (
+    "dept_no" VARCHAR(10)   NOT NULL,
+    "emp_no" INT   NOT NULL,
+    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
+        "dept_no","emp_no"
      )
 );
---Step02 import related csv file using pgAdmin "tables > Import/Export and so on..."
---Step03 view table
-SELECT * FROM titles;
+--import table dept_manager
+--view table
+SELECT * FROM dept_manager
 
---Step04 create table------------------------------------------------------------------------------
+--create table-------------------------------------------------------------------------------------
 CREATE TABLE "departments" (
     "dept_no" VARCHAR(10)   NOT NULL,
     "dept_name" VARCHAR(30)   NOT NULL,
@@ -28,16 +28,15 @@ CREATE TABLE "departments" (
         "dept_no"
      )
 );
---Step05 import related csv file using pgAdmin "tables > Import/Export and so on..."
---Step06 view table
-SELECT * FROM departments;
+--import table departments
+--view table
+SELECT * FROM departments
 
-
---Step07 create table------------------------------------------------------------------------------
+--create table-------------------------------------------------------------------------------------
 CREATE TABLE "employees" (
-    "emp_no" INTEGER   NOT NULL,
+    "emp_no" INT   NOT NULL,
     "emp_title_id" VARCHAR(10)   NOT NULL,
-    "birth_date" VARCHAR(10)   NOT NULL,     -- [MM-DD-YYYY] 10character max
+    "birth_date" VARCHAR(10)   NOT NULL,
     "first_name" VARCHAR(20)   NOT NULL,
     "last_name" VARCHAR(20)   NOT NULL,
     "sex" VARCHAR(1)   NOT NULL,
@@ -46,45 +45,48 @@ CREATE TABLE "employees" (
         "emp_no"
      )
 );
+--import table employees
+--view table
+SELECT * FROM employees
 
---Step08 import related csv file using pgAdmin "tables > Import/Export and so on..."
---Step09 view table
-SELECT * FROM employees;
-
-
---Step10 create table------------------------------------------------------------------------------
-CREATE TABLE "dept_manager" (
-    "dept_no" VARCHAR(10)   NOT NULL,
-    "emp_no" INTEGER   NOT NULL
-);
---Step11 import related csv file using pgAdmin "tables > Import/Export and so on..."
---Step12 view table
-SELECT * FROM dept_manager;
-
-
---Step13 create table------------------------------------------------------------------------------
-CREATE TABLE "salaries" (
-    "emp_no" INTEGER   NOT NULL,
-    "salary" INTEGER   NOT NULL
-);
---Step14 import related csv file using pgAdmin "tables > Import/Export and so on..."
---Step15 view table
-SELECT * FROM salaries;
-
-
---Step14 create table------------------------------------------------------------------------------
+--create table-------------------------------------------------------------------------------------
 CREATE TABLE "dept_emp" (
-    "emp_no" INTEGER   NOT NULL,
-    "dept_no" VARCHAR(10)   NOT NULL
+    "emp_no" INT   NOT NULL,
+    "dept_no" VARCHAR(10)   NOT NULL,
+    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
+        "emp_no","dept_no"
+     )
 );
---Step15 import related csv file using pgAdmin "tables > Import/Export and so on..."
---Step16 view table
-SELECT * FROM dept_emp;
+--import table dept_emp
+--view table
+SELECT * FROM dept_emp
+
+--create table-------------------------------------------------------------------------------------
+CREATE TABLE "salaries" (
+    "emp_no" INT   NOT NULL,
+    "salary" INT   NOT NULL,
+    CONSTRAINT "pk_salaries" PRIMARY KEY (
+        "emp_no"
+     )
+);
+--import table salaries
+--view table
+SELECT * FROM salaries
+
+--create table-------------------------------------------------------------------------------------
+CREATE TABLE "titles" (
+    "title_id" VARCHAR(10)   NOT NULL,
+    "title" VARCHAR(30)   NOT NULL,
+    CONSTRAINT "pk_titles" PRIMARY KEY (
+        "title_id"
+     )
+);
+--import table titles
+--view table
+SELECT * FROM titles
 
 
---Step17 assign fk with reference to another table-------------------------------------------------
-ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
-REFERENCES "titles" ("title_id");
+
 
 ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
 REFERENCES "departments" ("dept_no");
@@ -92,12 +94,15 @@ REFERENCES "departments" ("dept_no");
 ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
 
-ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
+ALTER TABLE "departments" ADD CONSTRAINT "fk_departments_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "dept_emp" ("dept_no");
+
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "titles" ("title_id");
 
 ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
 
-ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "departments" ("dept_no");
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
 
